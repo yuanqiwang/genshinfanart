c860d8ad53fb7de894d7ff43d7713702b3a36896
Yuanqi Wang <zoeywang.zw@gmail.com>
2022-02-22 00:45:55 -0600

    Added detail post page, dynamtic router for categories and posts, added top carousel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '../components/index.js'
 import {getPosts} from '../services'
-
+import { FeaturedPosts } from '../sections'
 // const posts = [
 //   {title: 'Childe', excerpt: 'Childe Bio'},
 //   {title: 'Yae Miko', excerpt: 'Yae Miko Bio'},
@@ -24,11 +24,8 @@ export async function getStaticProps() {
 export default function Home({ postData }) {
   return (
     <div className='container mx-auto px-10 mb-8'>
-      {/* <Head>
-        <title>Genshin WHY NOTHING</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head> */}
 
+      <FeaturedPosts />
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
           {postData.map((post, index) => <PostCard post={post.node} key={index} />)}
